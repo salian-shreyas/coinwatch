a29f31e07399882d919812ee18385c931cc1deb4
Shreyas G Salian <69745409+salian-shreyas@users.noreply.github.com>
2025-07-18 21:55:54 +0530

    Fetch coins and list on home-page
    Fetch top 100 coins with marketcap from the API

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-import axios from "axios";
-import express from "express";
-import bodyParser from "body-parser";
-import "dotenv/config"
+import axios from 'axios';
+import express, { response } from 'express';
+import bodyParser from 'body-parser';
+import 'dotenv/config';
+
+const API_URL = 'https://api.coingecko.com/api/v3/'
+const API_KEY = process.env.COINGECKO_API_KEY;
 
 const port = 3000;
 const app = express();
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
+app.get("/", async (req, res) => {
+    try {
+        const response = await axios.get(
+            API_URL + 'coins/markets',
+            {
+                headers: { 'x-cg-demo-api-key': API_KEY },
+                params: {
+                    'vs_currency': 'inr',
+                    'price_change_percentage': '24h',
+                },
+            }
+        );
+        res.render("index.ejs", { coins: response.data });
+    } catch (error) {
+       console.log(error); 
+       res.sendStatus(404);
+    }  
 });
 
 app.listen(port, () => {
